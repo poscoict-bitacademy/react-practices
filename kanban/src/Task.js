3bb0665007fac94c408aebdb38d461e8385634ad
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-03-18 15:21:13 +0900

    과제2

--- kanban/src/Task.js
+++ kanban/src/Task.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './assets/css/Task.css';
 
 const Task = ({name, done}) => {
+  const [stateDone, setStateDone] = useState(done);
+
   return (
     <li className={styles.TaskList__Task}>
-        <input type='checkbox' checked={done} />
+        <input
+          type='checkbox'
+          checked={stateDone}
+          onClick={e => setStateDone(!stateDone)} />
         {name}
         <a href='#' className={styles.TaskList__Task__remove}></a>
     </li>
