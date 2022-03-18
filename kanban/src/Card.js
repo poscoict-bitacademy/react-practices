55487c37deb61772bae9007de464a2a5e949dd4a
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-03-18 09:50:04 +0900

    state #3

--- kanban/src/Card.js
+++ kanban/src/Card.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './assets/scss/Card.scss';
 import TaskList from './TaskList';
 
 const Card = ({title, description, tasks}) => {
+  const [showDetails, setShowDetails] = useState(false);
+  
   return (
     <div className={styles.Card}>
         <div className={styles.Card__Title}>{title}</div>
