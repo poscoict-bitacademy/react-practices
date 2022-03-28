34187bd1fbbf9223d993ac93ac3c75002e30385f
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-03-28 10:54:32 +0900

    kanbanboard07 #1

--- emaillist07/backend/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ emaillist07/backend/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -3,8 +3,6 @@ package com.example.demo.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,9 +11,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.dto.JsonResult;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	private static final Log LOGGER = LogFactory.getLog(GlobalExceptionHandler.class);
 	
 	@ExceptionHandler(Exception.class)
 	@ResponseBody
@@ -23,7 +23,7 @@ public class GlobalExceptionHandler {
 		//1. 로깅
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
-		LOGGER.error(errors.toString());
+		log.error(errors.toString());
 		
 		//2. JSON 응답
 		return ResponseEntity.status(HttpStatus.OK).body(JsonResult.fail(errors.toString()));
--- kanbanboard07/frontend/src/KanbanBoard.js
+++ kanbanboard07/frontend/src/KanbanBoard.js
@@ -4,7 +4,6 @@ import styles from './assets/css/KanbaBoard.css';
 import cards from './assets/json/data.json';
 
 const KanbanBoard = () => {
-  // console.log([0, 1, 2, 3, 4].filter(e => e % 2 === 0));
   return (
     <div className={styles.KanbanBoard}>
       <CardList title={'ToDo'} cards={cards.filter(e => e.status === 'ToDo')} />
