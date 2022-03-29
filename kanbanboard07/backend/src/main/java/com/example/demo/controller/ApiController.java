6b0f8c68a66c570077049fc8f148847d6b5611ba
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-03-29 09:57:00 +0900

    kanbanboard#2

--- kanbanboard07/backend/src/main/java/com/example/demo/controller/ApiController.java
+++ kanbanboard07/backend/src/main/java/com/example/demo/controller/ApiController.java
@@ -1,8 +1,25 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ApiController {
+import com.example.demo.dto.JsonResult;
+import com.example.demo.repository.CardRepository;
 
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+	@Autowired
+	private CardRepository cardRepository;
+	
+	@GetMapping("/card")
+	public ResponseEntity<JsonResult> readCard() {
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(JsonResult.success(cardRepository.findAll()));
+	}
 }
--- kanbanboard07/frontend/src/Task.js
+++ kanbanboard07/frontend/src/Task.js
@@ -2,13 +2,11 @@ import React, {useState} from 'react'
 import styles from './assets/css/Task.css';
 
 const Task = ({name, done}) => {
-  const [stateDone, setStateDone] = useState(done);
-
   return (
     <li className={styles.TaskList__Task}>
         <input
           type='checkbox'
-          checked={stateDone}
+          checked={done === 'Y'}
           onClick={e => setStateDone(!stateDone)} />
         {name}
         <a href='#' className={styles.TaskList__Task__remove}></a>
