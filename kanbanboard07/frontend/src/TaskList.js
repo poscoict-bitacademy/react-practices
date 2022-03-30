9d302120f1c2e5ac4fc8c3b04bf65f93dde6fa36
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-03-30 12:57:05 +0900

    immutability #final

--- kanbanboard07/frontend/src/TaskList.js
+++ kanbanboard07/frontend/src/TaskList.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import styles from './assets/css/TaskList.css';
+import PropTypes from 'prop-types';
 import Task from './Task';
+import styles from './assets/css/TaskList.css';
 
-const TaskList = ({tasks}) => {
-  return (
-    <div className={styles.TaskList}>
-        <ul>
-            {tasks.map(task => <Task
-                                    key={task.no}
-                                    name={task.name}
-                                    done={task.done} /> )}
+export default function TaskList({cardNo, tasks, notifyAddTask, notifyChangeTaskDone}) {
+    return (
+        <div className='TaskList'>
+            <ul>
+                {tasks.map(task => <Task
+                                        key={task.no}
+                                        no={task.no}
+                                        name={task.name} 
+                                        done={task.done}
+                                        notifyChangeTaskDone={notifyChangeTaskDone} />)}
+            </ul>
             <input
-              type='text'
-              className={styles.TaskList__add_task}
-              placeholder={'태스크 추가'}
-              onKeyPress={e => {
-                if(e.key === 'Enter') {
-                  console.log(`call notifyAddTask(${e.target.value})`);
-                }
-              }}/>
-        </ul>
-    </div>     
-  );
+                type='text'
+                className={styles.TaskList__add_task}
+                placeholder='태스크 추가'
+                onKeyPress={(e) => {
+                    if (e.key === 'Enter') {
+                        notifyAddTask(e.target.value);
+                        e.target.value = '';
+                    }
+                }}/>
+        </div>
+    );
 }
 
-export default TaskList
+TaskList.propTypes = {
+    tasks: PropTypes.arrayOf(PropTypes.shape(Task.propTypes))
+}
