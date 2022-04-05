44bef1196711af80f3886ed387aab12180011197
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-04-05 14:09:47 +0900

    'final'

--- emaillist07/backend/src/main/java/com/example/demo/controller/ApiController.java
+++ emaillist07/backend/src/main/java/com/example/demo/controller/ApiController.java
@@ -3,9 +3,11 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,8 @@ import com.example.demo.repository.EmaillistRepository;
 import com.example.demo.vo.EmaillistVo;
 
 @RestController
+// @CrossOrigin(origins="*", allowedHeaders="*", allowCredentials="false", methods={RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS, RequestMethod.HEAD})
+@CrossOrigin
 public class ApiController {
 	
 	@Autowired
